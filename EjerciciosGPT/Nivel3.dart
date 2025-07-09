eec0cfdb628b4a749edb61e88bd7e3d19e97ac5e
FabrizioOMG7 <fabriziohoyosob@gmail.com>
2025-07-09 01:06:00 -0500

    Actualización Nivel3

--- EjerciciosGPT/Nivel3.dart
+++ EjerciciosGPT/Nivel3.dart
@@ -6,5 +6,10 @@ final nuevosNumeros = numeros.where((numero) => numero >18).toList();
 
 print('Los números mayores a 18 son: $nuevosNumeros');
 
+List<String> nombres = ['Fabrizio', 'Messi', 'Neymar', 'Lamine', 'Pedri'];
+print('Los nombres originales son: ${nombres.join(', ')}');
+
+List<String> nombresInvertidos = nombres.reversed.toList();
+print('Los nombres invertidos son: ${nombresInvertidos.join(', ')}');
 
 }
