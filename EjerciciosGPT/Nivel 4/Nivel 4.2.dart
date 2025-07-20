9e1cefba8c2476d18052ce5b643e8ca21820e7a5
FabrizioOMG7 <fabriziohoyosob@gmail.com>
2025-07-19 23:03:24 -0500

    Ejercicio4.2
    implementado

--- EjerciciosGPT/Nivel 4/Nivel 4.2.dart
+++ EjerciciosGPT/Nivel 4/Nivel 4.2.dart
@@ -10,6 +10,17 @@ Map<String, dynamic> AlumNotas = {
   'Ana' : [15,17,14] , 'Luis' : [20,18,19]
 };  
 
-int notas = AlumNotas
+List <int> notasAna = List<int>.from(AlumNotas['Ana']);
+List <int> notasLuis = List<int>.from(AlumNotas['Luis']);
+
+int sumaAna = notasAna.reduce((value, element) => value + element);
+int cantidadAna = notasAna.length;
+
+int sumaLuis = notasLuis.reduce((value, element) => value + element);
+int cantidadLuis = notasLuis.length;
+
+
+print('El promedio de notas de Ana es: ${sumaAna/cantidadAna}');
+print('El promedio de notas de Luis es: ${sumaLuis/cantidadLuis}');
 
 }
